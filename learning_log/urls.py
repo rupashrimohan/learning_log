1f74a549c935963c038dcd426f797327ae3b140d
Rupashri Mohan <47578871+rupashrimohan@users.noreply.github.com>
2026-09-24 14:50:39 -0400

    add topic and entry pages (#3)

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -5,4 +5,10 @@ from django.urls import path
 from . import views
 
 app_name = "learning_log"
-urlpatterns = [path("", views.index, name="index")]  # Home Page
+urlpatterns = [
+    path("", views.index, name="index"),  # Home Page
+    path("topics/", views.topics, name="topics"),  # Topics page
+    path(
+        "topics/<int:topic_id>/", views.topic, name="topic"
+    ),  # Detail page for single topic
+]
--- learning_log/views.py
+++ learning_log/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Topic
+from django.http import Http404
 
 # Create your views here.
 
@@ -6,3 +8,21 @@ from django.shortcuts import render
 def index(request):
     """The Home page for Learning Log."""
     return render(request, "learning_log/index.html")
+
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "learning_log/topics.html", context)
+
+
+def topic(request, topic_id):
+    """Show the specific topic details"""
+    try:
+        topic = Topic.objects.get(id=topic_id)
+    except Topic.DoesNotExist:
+        raise Http404("Topic does not exist.")
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_log/topic.html", context)
